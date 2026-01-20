e2a0964abc875613e71408763cc5ec3e5841dc5d
Aryan Bansal <aryan.bansal@contentstack.com>
2026-01-20 14:00:23 +0530

    Update api-route.js

--- pages/api/api-route.js
+++ pages/api/api-route.js
@@ -6,5 +6,5 @@ export default function handler(_, res) {
   res.setHeader('Set-Cookie', 'myCookie=myValue; Path=/; HttpOnly');
   res
     .status(200)
-    .json({ deploymentIdentifier: "contentfly-nginx-service-azure-na-prod-deploy:50039d933506-d23c1c5cab3e:run-counter-1" })
+    .json({ deploymentIdentifier: "This is test identifier message" })
 }
